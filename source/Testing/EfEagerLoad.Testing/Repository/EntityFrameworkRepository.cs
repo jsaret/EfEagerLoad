ab8065dd2f51a9b658baf75cd4e99b7e3e934146
jsaret <jsaret@gmail.com>
2020-02-03 02:41:00 +0200

    Started adding README.md Documentation (added repository method to match documentation)

--- source/Testing/EfEagerLoad.Testing/Repository/EntityFrameworkRepository.cs
+++ source/Testing/EfEagerLoad.Testing/Repository/EntityFrameworkRepository.cs
@@ -16,17 +16,22 @@ namespace EfEagerLoad.Testing.Repository
             _dbContext = dbContext;
         }
 
-        public async Task<IList<TEntity>> GetAll<TEntity>(bool eagerLoad = true, bool includeDeleted = false) where TEntity : class
+        public async Task<IList<TEntity>> GetAll<TEntity>(bool eagerLoad = true) where TEntity : class
         {
             return await _dbContext.Set<TEntity>().EagerLoad(_dbContext, eagerLoad).ToListAsync();
         }
 
-        public async Task<IList<TEntity>> GetAllMatching<TEntity>(Expression<Func<TEntity, bool>> predicate, bool eagerLoad = true, bool includeDeleted = false) where TEntity : class
+        public async Task<IList<TEntity>> GetAllMatching<TEntity>(Expression<Func<TEntity, bool>> predicate, bool eagerLoad = true) where TEntity : class
         {
             return await _dbContext.Set<TEntity>().Where(predicate).EagerLoad(_dbContext, eagerLoad).ToListAsync();
         }
 
-        public IQueryable<TEntity> Query<TEntity>(bool eagerLoad = false, bool includeDeleted = false) where TEntity : class
+        public async Task<TEntity> GetFirstMatching<TEntity>(Expression<Func<TEntity, bool>> predicate, bool eagerLoad = true) where TEntity : class
+        {
+            return await _dbContext.Set<TEntity>().Where(predicate).EagerLoad(_dbContext, eagerLoad).FirstOrDefaultAsync();
+        }
+
+        public IQueryable<TEntity> Query<TEntity>(bool eagerLoad = false) where TEntity : class
         {
             return _dbContext.Set<TEntity>().EagerLoad(_dbContext, eagerLoad).AsQueryable();
         }
--- source/Testing/EfEagerLoad.Testing/Repository/IRepository.cs
+++ source/Testing/EfEagerLoad.Testing/Repository/IRepository.cs
@@ -8,11 +8,12 @@ namespace EfEagerLoad.Testing.Repository
 {
     public interface IRepository
     {
-        Task<IList<TEntity>> GetAll<TEntity>(bool eagerLoad = true, bool includeDeleted = false) where TEntity : class;
+        Task<IList<TEntity>> GetAll<TEntity>(bool eagerLoad = true) where TEntity : class;
 
-        Task<IList<TEntity>> GetAllMatching<TEntity>(Expression<Func<TEntity, bool>> predicate, bool eagerLoad = true,
-                                                        bool includeDeleted = false) where TEntity : class;
+        Task<IList<TEntity>> GetAllMatching<TEntity>(Expression<Func<TEntity, bool>> predicate, bool eagerLoad = true) where TEntity : class;
 
-        IQueryable<TEntity> Query<TEntity>(bool eagerLoad = true, bool includeDeleted = false) where TEntity : class;
+        Task<TEntity> GetFirstMatching<TEntity>(Expression<Func<TEntity, bool>> predicate, bool eagerLoad = true) where TEntity : class;
+
+        IQueryable<TEntity> Query<TEntity>(bool eagerLoad = true) where TEntity : class;
     }
 }
